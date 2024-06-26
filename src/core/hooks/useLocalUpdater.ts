f1e3e2ade3cd2c3cd285722d043183c675db233d
GunNapat <napat.wat@innovasive.co.th>
2024-06-27 06:00:20 +0700

    update: default setup

--- src/core/hooks/useLocalUpdater.ts
+++ src/core/hooks/useLocalUpdater.ts
@@ -68,6 +68,7 @@ export const useLocalUpdater = (game: string) => {
             artifacts: [],
             builds: [],
             characters: [],
+            setup: [],
           })
         )
     }
